b6fda370eb32155f6cce5514f75e3cccba4ca4b0
Xin Meng <mengxin.city@gmail.com>
2018-04-21 21:30:07 +0100

    Initial commit

---
+++ src/main/java/science/mengxin/springbootjokes/SpringBootJokesApplication.java
@@ -0,0 +1,12 @@
+package science.mengxin.springbootjokes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJokesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJokesApplication.class, args);
+    }
+}
