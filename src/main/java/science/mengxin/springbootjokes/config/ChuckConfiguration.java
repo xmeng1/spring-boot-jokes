0bfbfe0cc3a5aacc5a0582a921f2f6a31482ef3b
Xin Meng <mengxin.city@gmail.com>
2018-04-22 09:38:17 +0100

    use configuration to config the dependency and use it in the service

---
+++ src/main/java/science/mengxin/springbootjokes/config/ChuckConfiguration.java
@@ -0,0 +1,20 @@
+package science.mengxin.springbootjokes.config;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * <p>Date:    22/04/18
+ *
+ * @author mengxin
+ * @version 1.0
+ */
+@Configuration
+public class ChuckConfiguration {
+
+    @Bean
+    public ChuckNorrisQuotes chuckNorrisQuotes() {
+        return new ChuckNorrisQuotes();
+    }
+}
--- src/main/java/science/mengxin/springbootjokes/services/JokeServiceImpl.java
+++ src/main/java/science/mengxin/springbootjokes/services/JokeServiceImpl.java
@@ -14,8 +14,8 @@ public class JokeServiceImpl implements JokeService {
 
     private final ChuckNorrisQuotes chuckNorrisQuotes;
 
-    public JokeServiceImpl() {
-        chuckNorrisQuotes = new ChuckNorrisQuotes();
+    public JokeServiceImpl(ChuckNorrisQuotes chuckNorrisQuotes) {
+        this.chuckNorrisQuotes = chuckNorrisQuotes;
     }
 
     @Override
